c2aeb05bc3697731b34711ed0dfa39bf84c2bc6e
iamgodot <xugodot@gmail.com>
2023-08-16 15:48:23 -0700

    Feat: update api data

--- src/services/backendApi.js
+++ src/services/backendApi.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-const API_PREFIX = "api/v1";
+const API_PREFIX = "/api/v1";
 
 export async function getUser() {
   const accessToken = localStorage.getItem("accessToken");
   const response = await axios.get(`${API_PREFIX}/user`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+  return response.data;
+}
+export async function getUserById(userId) {
+  const accessToken = localStorage.getItem("accessToken");
+  const response = await axios.get(`${API_PREFIX}/users/${userId}`, {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
   return response.data;
@@ -15,5 +22,17 @@ export async function createPost(payload) {
   const response = await axios.post(`${API_PREFIX}/posts`, payload, {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
+  return response;
+}
+
+export async function updateUser({ nickname, bio }) {
+  const accessToken = localStorage.getItem("accessToken");
+  const response = await axios.patch(
+    `${API_PREFIX}/user`,
+    { nickname, bio },
+    {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    }
+  );
   return response;
 }
--- src/services/mockApi.js
+++ src/services/mockApi.js
@@ -1,9 +1,23 @@
 import samplePDF from "./imm5818e.pdf";
+
+export function getUser() {
+  return {
+    username: "Godot",
+    nickname: "Godot",
+    email: "xugodot@gmail.com",
+    avatar: "https://i.pravatar.cc/150?img=2",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet. Etiam dictum dapibus ultricies. Sed vel aliquet libero. Nunc a augue fermentum, pharetra ligula sed, aliquam lacus.",
+  };
+}
 export function getFeeds() {
   return [
     {
       id: 1,
-      author: { nickname: "Godot", avatar: "https://i.pravatar.cc/150?img=2" },
+      author: {
+        id: 1,
+        nickname: "Godot",
+        avatar: "https://i.pravatar.cc/150?img=2",
+      },
       description:
         "Requirements of applying for a study permit in Canada: 1. Passport 2.  Offer letter 3. Study plan",
       file_url: samplePDF,
@@ -12,7 +26,11 @@ export function getFeeds() {
     },
     {
       id: 2,
-      author: { nickname: "Mike", avatar: "https://i.pravatar.cc/150?img=3" },
+      author: {
+        id: 2,
+        nickname: "Mike",
+        avatar: "https://i.pravatar.cc/150?img=3",
+      },
       description:
         "Requirements of applying for a study permit in Canada: 1. Passport 2.  Offer letter 3. Study plan 4. Foobar",
       file_url: samplePDF,
